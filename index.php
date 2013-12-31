3084b6863d517653f2063ef51c608cfa87d6167d
Josef Zacek <info@josefzacek.com>
2013-12-31 20:09:25 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Josef Zacek - website starter pack</title>
+  <meta name="author" content="www.josefzacek.com">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="images/favicon.ico" />
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+<!--[if lt IE 8]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<div id="wrapper">
+
+
+</div>
+<!-- wrapper end -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/css3-mediaqueries.js"></script>
+</body>
+</html>
+
